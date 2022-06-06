8537c0d48f5def4ed7bf42a03d51d6801666bdd4
Kuba Marek <blue.cube@seznam.cz>
2022-06-06 23:03:59 +0200

    assembler: add header to error messages

--- assembler/printing.c
+++ assembler/printing.c
@@ -4,22 +4,26 @@
 #include <stdio.h>
 
 int error(const char* format, ...) {
-    va_list ap;
-    va_start(ap, format);
-    int ret1 = vfprintf(stderr, format, ap);
+    int ret1 = fprintf(stderr, "error: ");
     if (ret1 < 0)
         return ret1;
-    va_end(ap);
 
-    int ret2 = fprintf(stderr, "\n");
+    va_list ap;
+    va_start(ap, format);
+    int ret2 = vfprintf(stderr, format, ap);
     if (ret2 < 0)
         return ret2;
+    va_end(ap);
 
-    return ret1 + ret2;
+    int ret3 = fprintf(stderr, "\n");
+    if (ret3 < 0)
+        return ret3;
+
+    return ret1 + ret2 + ret3;
 }
 
 int localized_error(struct location location, const char* format, ...) {
-    int ret1 = fprintf(stderr, "%s:%u:%u: ", location.filename, location.line, location.column);
+    int ret1 = fprintf(stderr, "%s:%u:%u: error: ", location.filename, location.line, location.column);
     if (ret1 < 0)
         return ret1;
 
