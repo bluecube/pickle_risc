be1104e821b4b43f3353b274dcda028953209b64
Kuba Marek <blue.cube@seznam.cz>
2022-10-13 23:06:13 +0200

    emulator: More microcode codegen WIP

--- emulator-rs/build.rs
+++ emulator-rs/build.rs
@@ -5,6 +5,7 @@ use std::io::Write;
 use std::path::Path;
 use std::collections::BTreeMap;
 use std::iter::repeat;
+use std::ops::Range;
 
 use serde::Deserialize;
 use itertools::Itertools;
@@ -14,14 +15,15 @@ use thiserror::Error;
 #[derive(Deserialize, Debug)]
 struct InstructionSet {
     instructions: BTreeMap<String, InstructionDef>,
-    //invalid_instruction: InstructionDef
+    invalid_instruction_microcode: Option<Vec<Vec<String>>>
 }
 
 #[derive(Deserialize, Debug)]
 struct InstructionDef {
     #[serde(default)]
     args: BTreeMap<String, InstructionEncodingArgType>,
-    encoding: Vec<InstructionEncodingPiece>
+    encoding: Vec<InstructionEncodingPiece>,
+    microcode: Option<Vec<Vec<String>>>
 }
 
 #[derive(Deserialize, Debug, Copy, Clone)]
@@ -50,7 +52,10 @@ enum InstructionDefinitionError {
     UndefinedArgument { mnemonic: String, arg_name: String },
 
     #[error("Instruction {mnemonic} has bad encoding length")]
-    WrongEncodingLength { mnemonic: String, bits: usize }
+    WrongEncodingLength { mnemonic: String, bits: usize },
+
+    #[error("Bad microcode for instruction {mnemonic}: {details}")]
+    MicrocodeError { mnemonic: String, details: String }
 }
 
 impl TryFrom<&str> for InstructionEncodingArgType {
@@ -158,13 +163,23 @@ fn generate_instruction_handler() -> anyhow::Result<()> {
 
     writeln!(target, "match opcode >> {} {{", instruction_bits - opcode_bits)?;
     let opcode_table = make_opcode_table(&definition, opcode_bits, instruction_bits)?;
-    for (count, (first_opcode, mnemonic)) in opcode_table.iter().enumerate().dedup_by_with_count(|x, y| x.1 == y.1) {
-        writeln!(target, "    {:#04x}{} => {{",
-            first_opcode,
-            if count > 1 { format!(" ..= {:#04x}", first_opcode + count - 1) } else { "".to_string() }
+    for (count, (first_opcode, instruction)) in opcode_table
+        .iter()
+        .enumerate()
+        .dedup_by_with_count(|x, y| x.1.map(|x| x.0) == y.1.map(|x| x.0))
+    {
+
+        generate_opcode_match_arm(
+            instruction.map(|x| x.0),
+            first_opcode..(first_opcode + count),
+            if let Some((_, instruction_def)) = instruction {
+                &instruction_def.microcode
+                // Unwrap is ok, because we got the menmonic from the encodings in the first place
+            } else {
+                &definition.invalid_instruction_microcode
+            },
+            &mut target
         )?;
-        writeln!(target, "        // {}", mnemonic.unwrap_or("invalid instruction"))?;
-        writeln!(target, "    }}")?;
     }
     writeln!(target, "    _ => unreachable!();")?;
     writeln!(target, "}}")?;
@@ -172,13 +187,13 @@ fn generate_instruction_handler() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn make_opcode_table(definition: &InstructionSet, opcode_bits: usize, instruction_bits: usize) -> anyhow::Result<Vec<Option<&str>>> {
-    let mut table: Vec<Option<&str>> = repeat(None).take(1 << opcode_bits).collect();
+fn make_opcode_table(definition: &InstructionSet, opcode_bits: usize, instruction_bits: usize) -> anyhow::Result<Vec<Option<(&str, &InstructionDef)>>> {
+    let mut table: Vec<Option<(&str, &InstructionDef)>> = repeat(None).take(1 << opcode_bits).collect();
 
     for (mnemonic, instruction_def) in &definition.instructions {
         let encoding = instruction_def.encoding(&mnemonic, instruction_bits)?;
         for opcode in expand_encoding(&encoding[..opcode_bits]) {
-            table[opcode] = Some(&mnemonic);
+            table[opcode] = Some((&mnemonic, &instruction_def));
         }
     }
 
@@ -191,4 +206,89 @@ fn expand_encoding(s: &str) -> impl Iterator<Item = usize> {
         .map(|c| match c { '0' => 0..=0, '1' => 1..=1, _ => 0..=1, })
         .multi_cartesian_product()
         .map(|x| x.iter().fold(0, |acc, digit| (acc << 1) + digit))
+}
+
+fn generate_opcode_match_arm(
+    mnemonic: Option<&str>,
+    opcodes: Range<usize>,
+    microcode: &Option<Vec<Vec<String>>>,
+    target: &mut fs::File
+) -> anyhow::Result<()> {
+    if opcodes.len() == 1 {
+        writeln!(target, "    {:#04x} => {{", opcodes.start)?;
+    } else {
+        writeln!(target, "    {:#04x} .. {:#04x} => {{", opcodes.start, opcodes.end)?;
+    }
+    writeln!(target, "        // {}", mnemonic.unwrap_or("invalid instruction"))?;
+    if let Some(microcode) = microcode {
+        for (i, microcode_step) in microcode.iter().enumerate() {
+            generate_microcode_step(i, microcode_step, target)?;
+        }
+    } else {
+        writeln!(target, "        todo!(); // Missing microcode!")?;
+    }
+    writeln!(target, "    }}")?;
+
+    Ok(())
+}
+
+fn generate_microcode_step(step: usize, microcode: &Vec<String>, target: &mut fs::File) -> anyhow::Result<()> {
+    let indent = "        ";
+
+    if step > 0 { writeln!(target)?; }
+    writeln!(target, "{}// Microcode step {}", indent, step)?;
+
+    let mut microinstructions = microcode.iter()
+        .map(|microinstruction| translate_microinstruction(microinstruction))
+        .collect::<Result<Vec<_>, _>>()?;
+
+    microinstructions.sort_by(|(_, priority1), (_, priority2)| priority1.cmp(priority2));
+
+    for (code, _) in microinstructions {
+        writeln!(target, "{}{}", indent, code)?;
+    }
+
+    // TODO: Conditionals in microcode
+    Ok(())
+}
+
+/// Parse a microinstruction, returns rust code to emulate it, and its priority
+/// (to produce microinstructions that produce value before the ones that consume them)
+fn translate_microinstruction(microinstruction: &str) -> Result<(String, usize), InstructionDefinitionError> {
+    let (code, priority) = match microinstruction {
+        "pc->left" => ("let left_bus = state.pc;", 0),
+        "pc->addr_base" => ("let addr_base_bus = state.pc;", 0),
+        "f2->left" => ("let left_bus = state.get_gpr(opcode & 0x7);", 0),
+        "f3->left" => ("let left_bus = state.get_gpr((opcode >> 3) & 0x7);", 0),
+        "f4->right" => ("let right_bus = state.get_gpr((opcode >> 6) & 0x7);", 0),
+        "f5->right" => ("let right_bus = state.get_gpr((opcode >> 10) & 0x7);", 0),
+        "f6->right" => ("let right_bus = state.get_cr((opcode >> 9) & 0x7);", 0),
+        "f7->right" => ("let right_bus = sign_extend((opcode >> 3) & 0xFF, 8);", 0),
+
+        "right->addr_base" => ("let addr_base_bus = right_bus;", 1),
+        "left->mem_data" => ("let mem_data = left_bus;", 1),
+
+        "alu_add->result" => ("let result_bus = left_bus + right_bus;", 1),
+        "alu_and->result" => ("let result_bus = left_bus & right_bus;", 1),
+        "alu_or->result" => ("let result_bus = left_bus | right_bus;", 1),
+        "alu_xor->result" => ("let result_bus = left_bus ^ right_bus;", 1),
+        "alu_sub->result" => ("let result_bus = left_bus - right_bus;", 1),
+        "alu_upsample->result" => ("let result_bus = (left_bus & 0xff) | (right_bus & 0xff) << 8;", 1),
+
+        "f8->addr_offset" => ("let mem_address = addr_base_bus + sign_extend((opcode >> 3) & 0x7F, 7);", 2),
+        "0->addr_offset" => ("let mem_address = addr_base_bus;", 2),
+        "1->addr_offset" => ("let mem_address = addr_base_bus + 1;", 2),
+
+        "mem_address->pc" => ("state.pc = mem_address;", 3),
+        "read_mem_data" => ("let mem_data = state.read_memory_virt(mem_address);", 3),
+        "write_mem_data" => ("state.write_memory_virt(mem_address, left_bus);", 3),
+
+        "mem_data->instruction" => ("state.next_opcode = mem_data;", 4),
+        "mem_data->result" => ("let result_bus = mem_data;", 4),
+
+        "result->f1" => ("state.set_gpr(opcode & 0x7, result_bus);", 5),
+        "result->f6" => ("state.set_cr((opcode >> 9) & 0x7, result_bus);", 5),
+        _ => ("todo!();", 9999)
+    };
+    Ok((format!("{} // {}", code, microinstruction), priority))
 }
