3668dabcb60b34bb39d530e1f5fa617a2c927176
Kuba Marek <blue.cube@seznam.cz>
2022-10-27 22:56:00 +0200

    emulator: WIP main.rs

--- emulator-rs/src/main.rs
+++ emulator-rs/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+mod cpu;
+mod cpu_types;
+mod memory;
+mod system;
+mod util;
+
+use crate::system::SystemState;
+use clap::Parser;
+
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+struct Cli {
+    /// Path to intel hex image of the boot rom
+    boot_rom_path: PathBuf,
+}
+
+fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
+
+    let _system = SystemState::new(cli.boot_rom_path)?;
+
+    Ok(())
 }
