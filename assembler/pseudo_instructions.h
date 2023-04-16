83d00f65c944553d0972459e135c9a76ef83ba97
Kuba Marek <blue.cube@seznam.cz>
2023-04-16 13:29:37 +0200

    Removed old code

--- assembler/pseudo_instructions.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-struct token;
-struct assembler_state;
-
-bool process_pseudo_instruction(struct token* mnemonicToken, struct assembler_state* state, struct tokenizer_state* tokenizer);
--- assembler/test_include.s
+++
@@ -1,5 +0,0 @@
-.section ".data"
-.dw 0, 0, 0, 0
-included_data:
-.dd 0x1eadbeef
-.dw 0, 0, 0, 0
